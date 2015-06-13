8a5da4dd80384767287aee62c2a098b9867d8713
Brett M. Story <brettmstory@gmail.com>
2015-06-12 21:40:38 -0500

    Depth controller now properly swaps layers.

--- MeetingGrey.Unity/Assets/Source/Depth/DepthController.cs
+++ MeetingGrey.Unity/Assets/Source/Depth/DepthController.cs
@@ -16,6 +16,11 @@
         private static DepthController _instance;
 
         /// <summary>
+        /// The offset of the background.
+        /// </summary>
+        private Vector3 _backgroundOffset = new Vector3(-1.0f, 0.5f, 1.0f);
+
+        /// <summary>
         /// The parent object of all background objects.
         /// </summary>
         private GameObject _backgroundParent;
@@ -70,6 +75,14 @@
             var newFirst = this._surfaceLayerMasks[1];
             this._surfaceLayerMasks[1] = this._surfaceLayerMasks[0];
             this._surfaceLayerMasks[0] = newFirst;
+
+            if (this._surfaceLayerMasks[0] == LayerConstants.SurfaceForegroundLayerMask) {
+                this._backgroundParent.transform.position += this._backgroundOffset;
+                this._foregroundParent.transform.position -= this._backgroundOffset;
+            } else {
+                this._backgroundParent.transform.position -= this._backgroundOffset;
+                this._foregroundParent.transform.position += this._backgroundOffset;
+            }
         }
 
         /// <summary>
@@ -85,6 +98,22 @@
 
             this._backgroundParent = GameObject.FindGameObjectWithTag(TagConstants.SurfaceBackgroundParent);
             this._foregroundParent = GameObject.FindGameObjectWithTag(TagConstants.SurfaceForegroundParent);
+
+            if (this._backgroundParent == null || this._foregroundParent == null) {
+                Debug.LogError("There must be one game object that has the background parent tag and one with the foreground parent tag.");
+            }
+
+            this._backgroundParent.transform.position += this._backgroundOffset;
+
+            var backgroundSurfaceColliders = this._backgroundParent.GetComponentsInChildren<EdgeCollider2D>();
+            for (int i = 0; i < backgroundSurfaceColliders.Length; i++) {
+                backgroundSurfaceColliders[i].gameObject.layer = LayerConstants.SurfaceBackgroundLayer;
+            }
+
+            var foregroundSurfaceColliders = this._foregroundParent.GetComponentsInChildren<EdgeCollider2D>();
+            for (int i = 0; i < foregroundSurfaceColliders.Length; i++) {
+                foregroundSurfaceColliders[i].gameObject.layer = LayerConstants.SurfaceForegroundLayer;
+            }
         }
 
         /// <summary>
